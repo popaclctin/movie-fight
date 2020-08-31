d3e6eb0c08b08d2685924b8051edcc92231d88e8
Claudiu Popa <popa.clctin@gmail.com>
2020-08-31 15:24:35 +0300

    Create div in which to show movie results

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
+
+  if (response.data.Error) {
+    return [];
+  }
   return response.data.Search;
 };
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
-  console.log(movies);
+  for (const movie of movies) {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+        <h1>${movie.Title}</h1>
+        <img src="${movie.Poster}">
+      `;
+
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
 const input = document.querySelector("input");
