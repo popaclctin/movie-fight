de8372061791e5df8afac62cae7f86add4717442
Claudiu Popa <popa.clctin@gmail.com>
2020-09-02 15:41:19 +0300

    Add onOptionSelect and inputValue function values to the config object

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ createAutoComplete({
       ${movie.Title} (${movie.Year})
     `;
   },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
 });
 
 const onMovieSelect = async (movie) => {
